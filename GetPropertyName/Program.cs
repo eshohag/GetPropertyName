e93b5bb2b648ce4e822a9e52a791e7ce1357771e
Md. Shohag Mia <shohag.mia@datasoft-bd.com>
2019-08-03 17:23:11 +0600

    Add project files.

---
+++ GetPropertyName/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace GetPropertyName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User aUser = new User();
+
+            string name = GetPropertyName(() => aUser.ContactNo);
+            Console.WriteLine(name);
+
+            Console.ReadLine();
+        }
+        public static string GetPropertyName<T>(Expression<Func<T>> propertyLambda)
+        {
+            var property = propertyLambda.Body as MemberExpression;
+
+            if (property == null)
+            {
+                throw new ArgumentException("You must pass a lambda of the form: '() => Class.Property' or '() => object.Property'");
+            }
+
+            return property.Member.Name;
+        }
+    }
+}
---
+++ GetPropertyName/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetPropertyName
+{
+    class User
+    {
+        public string Names { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+    }
+}
